9b1d61764086d72f20b938bf467b8e07709ad919
Thanapat Nawang <thanapattt.m@gmail.com>
2024-12-31 12:32:23 +0700

    Add: Hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+import SearchForm from "@/components/SearchForm";
+
+export default async function Home({ searchParams }: {
+  searchParms: Promise<{ query: string }>;
+}) {
+
+  const query = (await searchParams).query;
+
   return (
     <>
-      <h1 className="text-2xl">Home</h1>
+      <section className="blue_container">
+        <h1 className="heading">เก็บทุกงาน<br />ในชั้นเรียนไว้ที่เดียว</h1>
+        <p className="sub-heading !max-w-3xl">
+          จัดการโน้ต สรุปเนื้อหา และติดตามความก้าวหน้าของวิชาเรียนได้อย่างง่ายดาย
+        </p>
+
+        <SearchForm query={query} />
+      </section>
+
     </>
   );
 }
