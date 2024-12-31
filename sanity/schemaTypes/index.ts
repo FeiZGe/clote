3fe0cdaabc502d7037f452049e9989be84de9096
Thanapat Nawang <thanapattt.m@gmail.com>
2024-12-31 19:22:08 +0700

    Create: Schema

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,7 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import { author } from '@/sanity/schemaTypes/author'
+import { note } from '@/sanity/schemaTypes/note'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [author, note],
 }
